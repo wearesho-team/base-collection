7bd79ae2f936448555be5be0275e4cab8aeb626e
KartaviK <varkuta.roma@gmail.com>
2018-10-19 14:38:01 +0300

    Base implementation

---
+++ src/BaseCollection.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Wearesho;
+
+/**
+ * Class BaseCollection
+ * @package Wearesho
+ */
+abstract class BaseCollection extends \ArrayObject implements \JsonSerializable
+{
+    /**
+     * @param array $elements
+     * @param int $flags
+     * @param string $iteratorClass
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(array $elements = [], int $flags = 0, string $iteratorClass = \ArrayIterator::class)
+    {
+        foreach ($elements as $element) {
+            $this->validate($element);
+        }
+
+        parent::__construct($elements, $flags, $iteratorClass);
+    }
+
+    /**
+     * Override to customize type of your collection.
+     * Must return declared class name
+     *
+     * @return string
+     */
+    abstract public function type(): string;
+
+    /**
+     * @param mixed $value
+     *
+     * @return BaseCollection|static
+     * @throws \InvalidArgumentException
+     */
+    public function append($value): BaseCollection
+    {
+        $this->validate($value);
+
+        parent::append($value);
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $index
+     * @param mixed $value
+     *
+     * @return BaseCollection|static
+     * @throws \InvalidArgumentException
+     */
+    public function offsetSet($index, $value): BaseCollection
+    {
+        $this->validate($value);
+
+        parent::offsetSet($index, $value);
+
+        return $this;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return (array)$this;
+    }
+
+    /**
+     * @param mixed $object
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function validate($object): void
+    {
+        $needType = $this->type();
+
+        if (!$object instanceof $needType) {
+            throw new \InvalidArgumentException("Element " . get_class($object) . " must be instance of " . $needType);
+        }
+    }
+}
---
+++ tests/Unit/BaseCollectionTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Wearesho\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Wearesho\BaseCollection;
+
+/**
+ * Class BaseCollectionTest
+ * @package Wearesho\Tests\Unit
+ * @coversDefaultClass \Wearesho\BaseCollection
+ * @internal
+ */
+class BaseCollectionTest extends TestCase
+{
+    /** @var BaseCollection */
+    protected $fakeBaseCollection;
+
+    protected function setUp()
+    {
+        $this->fakeBaseCollection = new class extends BaseCollection
+        {
+            public function type(): string
+            {
+                return \stdClass::class;
+            }
+        };
+    }
+
+    public function testGetType(): void
+    {
+        $this->assertEquals(\stdClass::class, $this->fakeBaseCollection->type());
+    }
+
+    public function testSuccessInstantiate(): void
+    {
+        $this->assertEmpty($this->fakeBaseCollection);
+        $this->assertCount(0, $this->fakeBaseCollection);
+
+        $elements = [
+            new \stdClass(),
+            new \stdClass(),
+        ];
+        $this->fakeBaseCollection = new $this->fakeBaseCollection($elements);
+
+        $this->assertNotEmpty($this->fakeBaseCollection);
+        $this->assertCount(2, $this->fakeBaseCollection);
+    }
+
+    public function testFailedInstantiate(): void
+    {
+        $this->assertEmpty($this->fakeBaseCollection);
+        $this->assertCount(0, $this->fakeBaseCollection);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Element Exception must be instance of stdClass');
+
+        $this->fakeBaseCollection = new $this->fakeBaseCollection([new \Exception()]);
+    }
+
+    public function testSuccessAppend(): void
+    {
+        $this->assertEmpty($this->fakeBaseCollection);
+        $this->assertCount(0, $this->fakeBaseCollection);
+
+        $this->fakeBaseCollection->append(new \stdClass());
+        $this->fakeBaseCollection->append(new \stdClass());
+        $this->fakeBaseCollection->append(new \stdClass());
+
+        $this->assertNotEmpty($this->fakeBaseCollection);
+        $this->assertCount(3, $this->fakeBaseCollection);
+
+        $this->fakeBaseCollection
+            ->append(new \stdClass())
+            ->append(new \stdClass())
+            ->append(new \stdClass());
+
+        $this->assertCount(6, $this->fakeBaseCollection);
+    }
+
+    public function testFailedAppend(): void
+    {
+        $this->assertEmpty($this->fakeBaseCollection);
+        $this->assertCount(0, $this->fakeBaseCollection);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Element Exception must be instance of stdClass');
+
+        $this->fakeBaseCollection->append(new \Exception());
+    }
+
+    public function testSuccessOffsetSet(): void
+    {
+        $this->assertEmpty($this->fakeBaseCollection);
+        $this->assertCount(0, $this->fakeBaseCollection);
+
+        $this->fakeBaseCollection->offsetSet(0, new \stdClass());
+        $this->fakeBaseCollection->offsetSet('key', new \stdClass());
+        $this->fakeBaseCollection->offsetSet(1, new \stdClass());
+
+        $this->assertNotEmpty($this->fakeBaseCollection);
+        $this->assertCount(3, $this->fakeBaseCollection);
+        $this->assertArrayHasKey(0, $this->fakeBaseCollection);
+        $this->assertArrayHasKey('key', $this->fakeBaseCollection);
+        $this->assertArrayHasKey(1, $this->fakeBaseCollection);
+    }
+
+    public function testFailedOffsetSet(): void
+    {
+        $this->assertEmpty($this->fakeBaseCollection);
+        $this->assertCount(0, $this->fakeBaseCollection);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Element Exception must be instance of stdClass');
+
+        $this->fakeBaseCollection->offsetSet(0, new \Exception());
+    }
+
+    public function testJsonSerialize(): void
+    {
+        $this->fakeBaseCollection
+            ->append(new \stdClass())
+            ->append(new \stdClass())
+            ->append(new \stdClass());
+
+        $this->assertArraySubset(
+            [
+                new \stdClass(),
+                new \stdClass(),
+                new \stdClass(),
+            ],
+            $this->fakeBaseCollection->jsonSerialize()
+        );
+    }
+}
